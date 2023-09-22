b672c57f675c8835219a248002d5da9ec7f6c20d
fraciah <karfraciah@gmail.com>
2023-09-22 20:01:57 +0300

    Changed padding

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,12 +14,12 @@ export function Card({ term }){
           })
       }, [term])
     return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10 pt-3">
             {data.map((item) => {
                 return(
-                    <div key={item.id} className="border-slate-200 p-4 rounded-lg shadow-lg shadow-slate-500/50 cursor-pointer">
-                        <img src={item.webformatURL} alt={item.tags} className="w-full h-56 object-cover mb-2" />
-                        <h2 className="text-lg font-semibold mb-2 text-rose-500">Courtesy of {item.user}</h2>
+                    <div key={item.id} className="rounded-lg cursor-pointer">
+                        <img src={item.webformatURL} alt={item.tags} className="w-full h-64 object-cover mb-2 rounded-xl" />
+                        <h2 className="text-base mb-2 font-semibold text-gray-900">Courtesy of {item.user}</h2>
                         <div className="grid grid-cols-2 mb-5 gap-2">
                             <h6><span className="font-medium">Likes:</span> {item.likes}</h6>
                             <h5><span className="font-medium">Views:</span> {item.views}</h5>
