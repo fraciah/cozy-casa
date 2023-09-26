926d5d237ed9053ac8944072a9f7ff12fe4ec342
fraciah <karfraciah@gmail.com>
2023-09-26 15:41:14 +0300

    Added icons

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,9 +6,10 @@ import { BiLandscape } from "react-icons/bi"
 import { TbHomeStar } from "react-icons/tb"
 import { TbBeach } from "react-icons/tb"
 import { MdDesk } from "react-icons/md"
-import { MdOutlineFreeBreakfast } from "react-icons/md"
+import { LuSailboat } from "react-icons/lu"
 import { LiaSwimmingPoolSolid } from "react-icons/lia"
-import { GiIceland } from "react-icons/gi"
+import { MdKeyboardArrowRight } from "react-icons/md"
+import { LiaGolfBallSolid } from "react-icons/lia"
 
 export function Filters(){
     
@@ -20,42 +21,45 @@ export function Filters(){
         <>
         <div className="px-10 flex items-center pt-6 pb-4 flex-wrap">
             
-            <div className="flex gap-6 sm:gap-5 py-1 mr-5 text-gray-500">
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
+            <div className="flex gap-6 sm:gap-4 py-1 mr-1 text-gray-500">
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
                     <HiOutlineHomeModern className="text-3xl"/>
                     <h1 className="text-xs">Tiny homes</h1>
                 </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
                     <BsFire className="text-3xl"/>
                     <h1 className="text-xs">Trending</h1>
                 </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
                     <MdDesk className="text-3xl"/>
                     <h1 className="text-xs">Rooms</h1>
                 </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
                     <TbHomeStar className="text-3xl"/>
                     <h1 className="text-xs">Design</h1>
                 </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
                     <TbBeach className="text-3xl"/>
                     <h1 className="text-xs">Beachfront</h1>
                 </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
                     <BiLandscape className="text-3xl"/>
                     <h1 className="text-xs">Amazing views</h1>
                 </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
-                    <MdOutlineFreeBreakfast className="text-3xl"/>
-                    <h1 className="text-xs text-center">Bed & breakfasts</h1>
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
+                    <LuSailboat className="text-3xl"/>
+                    <h1 className="text-xs text-center">Boats</h1>
                 </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
-                    <GiIceland className="text-3xl"/>
-                    <h1 className="text-xs">Countryside</h1>
-                </div>
-                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-950 cursor-pointer">
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
                     <LiaSwimmingPoolSolid className="text-3xl"/>
                     <h1 className="text-xs">Amazing pools</h1>
+                </div>
+                <div className="flex flex-col items-center w-10 sm:w-20 hover:text-gray-800 cursor-pointer">
+                    <LiaGolfBallSolid className="text-3xl"/>
+                    <h1 className="text-xs">Golfing</h1>
+                </div>
+                <div className="flex flex-col items-center justify-center rounded-full cursor-pointer p-2  w-9 h-9 border hover:shadow-md transform transition duration-500 ease-in-out hover:scale-110">
+                    <MdKeyboardArrowRight className="text-2xl text-black"/>
                 </div>
             </div>
 
