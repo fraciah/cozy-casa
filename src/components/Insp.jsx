5cec7b410572f7e4be5e5a1871e32b39c5acf656
fraciah <karfraciah@gmail.com>
2023-09-30 22:50:37 +0300

    Added subcategories

--- src/components/Insp.jsx
+++ src/components/Insp.jsx
@@ -4,11 +4,15 @@ export function Insp(){
 
     const subcategories = ["Canmore", "Benalmadena", "Marbella", "Mijas", "Prescott", "Scottsdale", "Tucson", "Jasper", "Mountain View", "Devonport", "Mallacoota", "Ibiza", "Anaheim", "Monterey", "Paso Robles", "Santa Barbara", "Sonoma", "Show more"];
 
-    const subItems = ["Chalet rentals", "Apartment rentals", "Beachfront rentals", "House rentals", "Rentals with pools", "Pet-friendly rentals", "Cabin rentals", "Vacation rentals", "Vacation rentals", "Pet-friendly rentals", "Apartment rentals", "Apartment rentals", "Bungalow rentals", "Cottage rentals","House rentals","BABY"];
+    const subItems = ["Chalet rentals", "Apartment rentals", "Beachfront rentals", "House rentals","House rentals", "Rentals with pools", "Pet-friendly rentals", "Cabin rentals", "Vacation rentals", "Vacation rentals", "Pet-friendly rentals", "Apartment rentals", "Apartment rentals", "Bungalow rentals","House rentals", "Cottage rentals","House rentals"];
     
+    const support = ["Support","Help Center","AirCover","Anti-discrimination","Disability support","Cancellation options","Report neighborhood concern"];
+    const hosting = ["Hosting","Airbnb your home","AirCover for Hosts","Hosting resources","Community forum","Hosting responsibly","Airbnb-friendly apartments"];
+    const airbnb = ["Airbnb","Newsroom","New features","Careers","Investors","Gift cards","Airbnb.org emergency stays"];
 
     return(
         <>
+        <div className="border-b">
             <div className="px-10 pt-14 mt-7 bg-gray-100">
                 <h1 className="font-semibold text-2xl mb-3 text-gray-900">Inspiration for future getaways</h1>
 
@@ -24,15 +28,42 @@ export function Insp(){
                     ))}
                 </div>
 
-                <div className="grid grid-cols-6 gap-2 pt-4 pb-14">
+                <div className="grid grid-cols-6 gap-4 pt-8 pb-14">
                     {subcategories.map((subcategory, index) =>(
-                        <div key={index} className="font-medium p-2 text-gray-900 bg-pink-500">
-                            {subcategory}
+                        <div key={index} className="cursor-pointer">
+                            <div className={`font-medium text-gray-900 ${subcategories[index] === subcategories[subcategories.length - 1]? 'hover:underline': ''}`}>{subcategory}</div>
+                            <div className="text-gray-500">{subItems[index] ? subItems[index] : ''}</div>
                         </div>
                     ))}
                 </div>
 
             </div>
+        </div>
+
+        <div className="grid grid-cols-3 bg-gray-100">
+            <div className="py-10">
+                {support.map((item, index) => (
+                    <div key={index} className="px-10">
+                        <h5 className={`py-1 cursor-pointer ${index === 0? 'text-gray-900 font-semibold' : 'text-gray-700 hover:underline'}`}>{item}</h5>
+                    </div>
+                ))}
+            </div>
+            <div className="py-10">
+                {hosting.map((item, index) => (
+                    <div key={index} className="px-10">
+                        <h5 className={`py-1 cursor-pointer ${index === 0? 'text-gray-900 font-semibold' : 'text-gray-700 hover:underline'}`}>{item}</h5>
+                    </div>
+                ))}
+            </div>
+            <div className="py-10">
+                {airbnb.map((item, index) => (
+                    <div key={index} className="px-10">
+                        <h5 className={`py-1 cursor-pointer ${index === 0? 'text-gray-900 font-semibold' : 'text-gray-700 hover:underline'}`}>{item}</h5>
+                    </div>
+                ))}
+            </div>
+
+        </div>
         </>
     )
 }
