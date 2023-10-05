27eb314af3479735e82ef5e3fcdf91cf766b29a1
fraciah <karfraciah@gmail.com>
2023-10-05 15:18:20 +0300

    Making footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ import { FaSquareXTwitter } from "react-icons/fa6"
 
 export function Footer(){
     return(
-        <div className="px-10 bg-gray-100">
-            <div className="flex justify-between border-t py-5">
-
-                <div className="flex gap-2 text-base text-gray-700">
-                    <a href="https://github.com/fraciah?tab=repositories" className="text-gray-900 font-semibold" target="blank">Crafted with 💖 by Fraciah</a>
-                    <span>·</span>
+        <div className="sm:px-10 px-5 bg-gray-100">
+            <div className="grid sm:flex justify-between border-t py-3 space-y-1 sm:py-5">
+                <div className="grid space-y-1 sm:flex items-center sm:text-center">
+                    <a href="https://github.com/fraciah?tab=repositories" className="text-gray-900 font-semibold hover:underline" target="blank">Crafted with 💖 by Fraciah</a>
+                <div className="flex gap-1 sm:gap-2 text-sm sm:text-base text-gray-700">
+                    <span className="hidden sm:block ml-2">·</span>
                     <h5 className="hover:underline cursor-pointer">Terms </h5>
                     <span>·</span>
                     <h5 className="hover:underline cursor-pointer">Sitemap</h5>
@@ -19,17 +19,18 @@ export function Footer(){
                     <span>·</span>
                     <h5 className="hover:underline cursor-pointer">Your Privacy Choices</h5>
                 </div>
+                </div>
 
-                <div className="flex items-center gap-5 cursor-pointer text-gray-950">
+                <div className="flex items-center space-x-3 sm:space-x-5 cursor-pointer text-gray-950">
                     <div className="flex items-center gap-1">
                         <TbWorld/>
-                        <h5 className="hover:underline cursor-pointer">English (US)</h5>
+                        <h5 className="hover:underline cursor-pointer text-sm sm:text-base">English (US)</h5>
                     </div>
-                    <div className="flex gap-1">
+                    <div className="flex gap-0 sm:gap-1 items-center">
                         <h5>$</h5>
-                        <h5 className="hover:underline cursor-pointer">USD</h5>
+                        <h5 className="hover:underline cursor-pointer text-sm sm:text-base">USD</h5>
                     </div>
-                    <div className="flex gap-2 text-xl">
+                    <div className="flex gap-1 sm:gap-2 text-lg sm:text-xl">
                         <FaFacebookSquare/>
                         <FaSquareXTwitter/>
                         <FaSquareInstagram/>
